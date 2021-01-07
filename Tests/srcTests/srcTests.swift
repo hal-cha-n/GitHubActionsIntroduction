2547db2fa97478830450e1a38c333eed23b190a7
hal-cha-n <halstroemeria.tyan@gmail.com>
2021-01-08 02:55:18 +0900

    テストが成功するコード追加

--- Tests/srcTests/srcTests.swift
+++ Tests/srcTests/srcTests.swift
@@ -26,7 +26,7 @@ final class srcTests: XCTestCase {
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)
 
-        XCTAssertEqual(output, "Hello, world!\n")
+        XCTAssertEqual(output, "Hello, halchan!\n")
     }
 
     /// Returns path to the built products directory.
